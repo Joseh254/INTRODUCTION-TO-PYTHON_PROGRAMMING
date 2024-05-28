d1e686f6f7188f62f48c2991c575a0f7a02160d6
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-05-28 15:10:58 +0300

    add datatypes

---
+++ PYTHON_DATA_TYPES/main.py
@@ -0,0 +1,12 @@
+# python have the folllowing datatypes
+# Numeric data types: int, float, complex.
+# String data types: str.
+# Sequence types: list, tuple, range.
+# Binary types: bytes, bytearray, memoryview.
+# Mapping data type: dict.
+# Boolean type: bool.
+# Set data types: set, frozenset.
+
+number = 6  #this is a numeric datatype
+my_string = "this is a string"  #this is a string datatype
+
