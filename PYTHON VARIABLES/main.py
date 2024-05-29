ec9381c3b84866256592d53215293b1f449b3da1
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-05-29 13:41:28 +0300

    add print age'

--- PYTHON VARIABLES/main.py
+++ PYTHON VARIABLES/main.py
@@ -60,6 +60,7 @@ you can declare multiple variables in single statement
 """
 
 name,age,gender = "joseph",25,"male"
+print(age)
 
 
 # WRITE A PROGRAM IN PYTHON TO ADD TWO NUMBERS.USE  COMMENTS TO DOCUMENT YOUR WORK
