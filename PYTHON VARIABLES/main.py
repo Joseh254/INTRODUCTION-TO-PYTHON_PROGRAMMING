55fddd468374dacc49d74589fd6747d7eab396ae
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-05-29 14:09:39 +0300

    add example

--- PYTHON VARIABLES/main.py
+++ PYTHON VARIABLES/main.py
@@ -8,3 +8,5 @@ A variable is created the moment you first assign a value to it or initialize it
 Example"""
 
 my_name = "joseph"  #assigning a string 'joseph' to variable named my_name
+
+x = 57 # assigning an integer datatype
