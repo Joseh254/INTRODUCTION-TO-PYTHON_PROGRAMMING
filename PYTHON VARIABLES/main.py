f44e8342586428f7d52631cfb426d5f03f02d934
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-05-29 13:28:34 +0300

    add rules

--- PYTHON VARIABLES/main.py
+++ PYTHON VARIABLES/main.py
@@ -48,7 +48,7 @@ print(A)# will print 45 in terminal
 """
 A variable name must start with a letter or the underscore character
 A variable name cannot start with a number
-
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
 
 """
 
