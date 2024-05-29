5c739eac6b148244641ac247a40ede8aeb5a95c1
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-05-29 14:08:05 +0300

    add python variables

---
+++ PYTHON VARIABLES/main.py
@@ -0,0 +1,10 @@
+# variables are containers for storing data
+"""Creating Variables
+
+Python has no command for declaring a variable.
+
+A variable is created the moment you first assign a value to it or initialize it.
+
+Example"""
+
+my_name = "joseph"  #assigning a string 'joseph' to variable named my_name
