fc61595d055c9e96468bd933edf8f9edef0c3158
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-05-29 13:54:42 +0300

    add more commments

--- PYTHON COMMENTS/main.py
+++ PYTHON COMMENTS/main.py
@@ -1,3 +1,6 @@
 # Comments can be used to explain Python code in python file which makes the code readable
 # comments are not excecuted during code execution
+# to write a comment in python,we start with '#' followed by the comment it self
+# example
+# this is a comment
 
