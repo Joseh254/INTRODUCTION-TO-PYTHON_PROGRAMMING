874998a13fcc375a2a7f3704f118e96d14b5fc48
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-05-29 13:40:03 +0300

    --

--- PYTHON VARIABLES/main.py
+++ PYTHON VARIABLES/main.py
@@ -50,8 +50,15 @@ A variable name must start with a letter or the underscore character
 A variable name cannot start with a number
 A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
 Variable names are case-sensitive (age, Age and AGE are three different variables)
-
+A variable name cannot be any of the Python keywords.
 
 """
+
+"""
+you can declare multiple variables in single statement
+
+"""
+
+
 
 # WRITE A PROGRAM IN PYTHON TO ADD TWO NUMBERS.USE  COMMENTS TO DOCUMENT YOUR WORK
