f3de6894c419da1c8892c4ad31de59b9d823c681
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-05-30 19:04:59 +0300

    -

--- PYTHON NUMBERS/main.py
+++ PYTHON NUMBERS/main.py
@@ -1,1 +1,57 @@
 #****************PYTHON NUMBERS***************
+"""In python we have three numeric:
+
+1,int
+2,float
+3.complex
+
+"""
+
+# EXAMPLES
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+"""
+         INTEGERS
+Int, or integer, is a whole number, positive or negative, without decimals.
+
+"""
+
+#EXAPLES INCLUDE
+
+A =1
+b=100000000000
+
+#to verify the type of the variable, use keyword type
+print(type(A))
+
+
+"""      FLOAT
+Float "float is a number, positive or negative, containing one or more decimals.
+
+"""
+# EXAMPLES INCLUDE
+
+z = 1.03
+x = 5.09388378363566373
+
+print("")
+print(type(z))# verify the type of the variables
+
+# ******************COMPLEX***************
+
+# Complex numbers are written with a "j" as the imaginary part:
+x = 3J+5j
+y = 53j
+z = -57j
+
+print(type(x))
+print("")
+print(type(y))
+print("")
+print(type(z))
+
+# this will return the types as comples datatypes
+
