24ccde12233085cca8030d0d2a6ac2aff18c842a
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-05-30 13:37:53 +0300

    define global variable

--- PYTHON VARIABLES/main.py
+++ PYTHON VARIABLES/main.py
@@ -119,6 +119,8 @@ runExample() #this will print the value of y which is the string joseph
 
 # you can also change the value of a global variable the same way we do for local variables
 
+global mypet
+
 
 
 
