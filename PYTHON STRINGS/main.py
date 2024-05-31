3b2702b7d8020fde25b09fe43ffd442c75e0bfde
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-05-31 20:48:17 +0300

    add string example

--- PYTHON STRINGS/main.py
+++ PYTHON STRINGS/main.py
@@ -1,2 +1,6 @@
 # ******************************************PYTHON STRINGS****************************************
 
+# Strings are sequence of character sand they are sorounded by either single or double quotes
+# 'hjoseph' is the same as "joseph"
+
+my_name = "joseph"
