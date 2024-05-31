7a58d23342c0eaab8183b09baa12531c725d07c4
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-05-31 20:46:25 +0300

    demonstrate slicing from start

--- PYTHON STRINGS/PYTHON SLICING/main.py
+++ PYTHON STRINGS/PYTHON SLICING/main.py
@@ -1,1 +1,16 @@
-# 
+# slicing is the process of returnig range of character
+#Specify the start index and the end index, separated by a colon, to return a part of the string.
+#EXAMPLE
+ 
+b="hell0 word"
+print(b[2:5])
+# this prints llo since it returns eleent at index 2 to 4
+#note that index 5 is excluded
+
+
+# sliceing from start
+# we leave the first index but specify the index at which it will stop slicing
+# EXAMPLE
+b = "helllo word"
+
+print(b[:5])
