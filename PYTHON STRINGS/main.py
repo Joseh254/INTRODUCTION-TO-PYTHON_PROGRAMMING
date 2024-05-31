10263f93c434db18cb28bef9a7845fc74bd6e5fc
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-05-31 21:04:33 +0300

    looping through array

--- PYTHON STRINGS/main.py
+++ PYTHON STRINGS/main.py
@@ -28,3 +28,18 @@ Agent x
 # for this case we use either three single or double quotes
 
 print(a)
+
+#someone can be right to say strings are arrays and for this case square brackets ae used
+#to access elements of the string.
+
+a = "hello world"
+print(a[0]) #returns the first character in the string which id h
+#elements starts from index 0
+
+
+#*************************LOOPING THROUGH STRING*****************
+#Since strings are arrays, we can loop through the characters in a string, with a for loop.
+
+
+
+
