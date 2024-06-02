a7e0c29dadd05aaff1dd2256b47ac50a98f8a77c
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-06-02 23:04:35 +0300

    add lower()method in strings

--- PYTHON STRINGS/MODIFYING STRINGS/main.py
+++ PYTHON STRINGS/MODIFYING STRINGS/main.py
@@ -3,3 +3,12 @@
 # UPPER CASE
 
 #upper() method Is used to return the string in upper case:
+
+#EXAPMLE
+
+a = "Hello, World!"
+print(a.upper())
+
+
+#LOWERCASE
+#lower() method Is used to return the string in lower case:
