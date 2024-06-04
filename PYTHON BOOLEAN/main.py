5aa1f2e4e0d26cc741978cb026e9dfc3038b596d
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-06-04 13:19:20 +0300

    describe what is boolean in python

--- PYTHON BOOLEAN/main.py
+++ PYTHON BOOLEAN/main.py
@@ -1,1 +1,4 @@
 #PYTHON BOOLEANS
+# returns either of the two values that is "true" false
+# there are some cases you may need to check whether something is true or false
+#for example if the age of the user is above 18 years.
