d3a7e7321186b6e07412b03948558aced9a5edf0
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-06-04 13:41:53 +0300

    use exaple of string formating

--- PYTHON STRINGS/PYTHON FORMATING/main.py
+++ PYTHON STRINGS/PYTHON FORMATING/main.py
@@ -1,1 +1,10 @@
-# 
+# # python formating is done to allow compactibility with other data types
+# we can combine strings and numbers by using the format() method!
+
+# The format() method takes the passed arguments, formats them, and places them in the string where the placeholders {} are:
+
+#example
+age = 36
+name = "My name is John, and I am {} years old"
+print(name.format(age))
+
