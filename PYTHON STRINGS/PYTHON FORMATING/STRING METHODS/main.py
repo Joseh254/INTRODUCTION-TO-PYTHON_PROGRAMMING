116cdeeee207004f817ecdc337adc231eb90879b
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-06-04 13:22:59 +0300

    check if a string ends with a specified character

--- PYTHON STRINGS/PYTHON FORMATING/STRING METHODS/main.py
+++ PYTHON STRINGS/PYTHON FORMATING/STRING METHODS/main.py
@@ -4,5 +4,6 @@
 capitalize()	Converts the first character to upper case
 center()	Returns a centered string
 count()	Returns the number of occurence of a specified value  in a string
+endswith()	checks if the string ends with the specified value
 
 """
