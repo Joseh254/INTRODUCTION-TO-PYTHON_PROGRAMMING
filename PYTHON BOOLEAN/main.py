3e3dbb048370a56d501ff1d37ac5752d581019bc
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-06-04 13:24:32 +0300

    use an example of boolean

--- PYTHON BOOLEAN/main.py
+++ PYTHON BOOLEAN/main.py
@@ -2,3 +2,8 @@
 # returns either of the two values that is "true" false
 # there are some cases you may need to check whether something is true or false
 #for example if the age of the user is above 18 years.
+# example
+
+a =9
+b=6
+print(a>b)# this returns to true since 9 is greeter than b
