bcbaf8cf58b42ebe9f52c98604b63804450e00cd
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-06-04 13:21:11 +0300

    returning a centered string

--- PYTHON STRINGS/PYTHON FORMATING/STRING METHODS/main.py
+++ PYTHON STRINGS/PYTHON FORMATING/STRING METHODS/main.py
@@ -2,5 +2,6 @@
 # All string methods returns a new values. but do not modify the original string.
 
 capitalize()	Converts the first character to upper case
+center()	Returns a centered string
 
 """
