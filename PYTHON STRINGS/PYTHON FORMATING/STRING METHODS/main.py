2faf178fdebf262ae39d0558a236d5da7be8a5c1
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-06-04 13:28:30 +0300

    is title in python

--- PYTHON STRINGS/PYTHON FORMATING/STRING METHODS/main.py
+++ PYTHON STRINGS/PYTHON FORMATING/STRING METHODS/main.py
@@ -11,5 +11,6 @@ islower()	Returns True if all characters in the string are lower case
 isnumeric()	Returns True if all characters in the string are numeric
 isprintable()	Returns True if all characters in the string are printable
 isspace()	Returns True if all characters in the string are whitespaces
+istitle()	Returns True if the string follows the rules of a title
 
 """
