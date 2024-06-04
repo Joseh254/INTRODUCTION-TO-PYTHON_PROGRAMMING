838f6f11ddf798286e7c0102685a1440456d6b94
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-06-04 13:33:04 +0300

    join strings using + operator

--- STRING CONCATENATION/main.py
+++ STRING CONCATENATION/main.py
@@ -1,3 +1,9 @@
 #STRING CONCATENATION
 #refers to the prcess of joining steings together and return a single string.
 # one way to do this is by using + operator
+# for example
+
+a="joseph"
+b= "mbugua"
+full_name = a+b
+print(full_name)#retuns joseph mbugua
