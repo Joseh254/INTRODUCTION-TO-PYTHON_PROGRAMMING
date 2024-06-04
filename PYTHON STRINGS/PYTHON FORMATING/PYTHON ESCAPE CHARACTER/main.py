ef2f0a055bfa2ba23449f5bda11217ea602f8f53
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-06-04 13:57:32 +0300

    demonstrate on how to use invalid character inside a string

---
+++ PYTHON STRINGS/PYTHON FORMATING/PYTHON ESCAPE CHARACTER/main.py
@@ -0,0 +1,8 @@
+#PYTHON ESCAPE CHARACTER
+#python escape character is used to isert invalid characters 
+#in a stirng.
+#for example adding a double quote inside a string
+# for this case we use a backslash
+#EXAMPLE
+my_name ="joseph \"mbugua\"and i am a python programmer"
+print(my_name)
