fa6327d89e33481a8937b5d6d1a1f50e7885063c
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-06-04 13:20:35 +0300

    -

--- PYTHON STRINGS/PYTHON FORMATING/STRING METHODS/main.py
+++ PYTHON STRINGS/PYTHON FORMATING/STRING METHODS/main.py
@@ -1,1 +1,6 @@
-#STRING METHODS
+"""#STRING METHODS
+# All string methods returns a new values. but do not modify the original string.
+
+capitalize()	Converts the first character to upper case
+
+"""
