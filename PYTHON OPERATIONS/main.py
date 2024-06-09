34bd3b8d5c67c044b9158c1e4c4ada285181bb7e
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-06-09 21:36:45 +0300

    -

--- PYTHON OPERATIONS/main.py
+++ PYTHON OPERATIONS/main.py
@@ -12,5 +12,8 @@ Operator	Name	            Example
 +	        Addition	        x + y	
 -	        Subtraction	        x - y	
 *	        Multiplication	    x * y	
+/	        Division	        x / y	
+%	        Modulus	            x % y	
+
 
 """
