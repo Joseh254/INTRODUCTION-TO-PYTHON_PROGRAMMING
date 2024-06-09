f5f1547564da5cc09dd3b2f64dcfe7d66efbc7bd
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-06-09 23:18:15 +0300

    -

--- PYTHON OPERATIONS/main.py
+++ PYTHON OPERATIONS/main.py
@@ -91,6 +91,7 @@ Operator	Name	Description	                                            Example
 
 & 	        AND	     Sets each bit to 1 if both bits are 1	                x & y	
 |	        OR	     Sets each bit to 1 if one of two bits is 1             x | y	
-
+^	        XOR	     Sets each bit to 1 if only one of two bits is 1	    x ^ y	
+~	        NOT	     inverts all the bits	                                ~x
 
 """
