2f6b3cf6b962c6670b6fbbbe65f8aa5a14e1a752
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-06-09 21:11:26 +0300

    -

--- PYTHON OPERATIONS/main.py
+++ PYTHON OPERATIONS/main.py
@@ -64,6 +64,9 @@ Operator	Name	                    Example
 """
 
 
-    
+Operator	Description	                                                Example	
+and         Returns True if both statements are true	                x < 5 and  x < 10	
+or	        Returns True if one of the statements is true	            x < 5 or x < 4	
+not	        Reverse the result, returns False if the result is true     
 
 """
