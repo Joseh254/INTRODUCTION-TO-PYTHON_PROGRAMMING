1b2d11cb3a79dd492b47515cb4dc57179134320f
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-06-09 21:41:04 +0300

    -

--- PYTHON OPERATIONS/main.py
+++ PYTHON OPERATIONS/main.py
@@ -25,7 +25,9 @@ Operator	Name	            Example
 """
 Operator	Example	        Same As	
 =	        x = 5	        x = 5	
-+=	        x += 3	        x = x + 3	
++=	        x += 3	        x = x + 3
+-=	        x -= 3	        x = x - 3	
+
 
 
 """
