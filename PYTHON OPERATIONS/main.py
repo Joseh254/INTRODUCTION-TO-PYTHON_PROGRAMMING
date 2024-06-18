8fa0243d6671ec4ae2563c6ebf39e826770cb431
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-06-18 15:46:40 +0300

    add more operators

--- PYTHON OPERATIONS/main.py
+++ PYTHON OPERATIONS/main.py
@@ -112,7 +112,17 @@ print(15 + 5 * 3)
 Operator	                        Description	Try it
 ()	                                Parentheses	
 **	                                Exponentiation	
-
++x  -x  ~x	                        Unary plus, unary minus, and bitwise NOT	
+*  /  //  %	                        Multiplication, division, floor division, and modulus	
++  -	                            Addition and subtraction	
+<<  >>	                            Bitwise left and right shifts	
+&	                                Bitwise AND	
+^	                                Bitwise XOR	
+|	                                Bitwise OR	
+==  !=  >  >=  <  <=                is  is not  in  not in 	Comparisons, identity, and membership operators	
+not	                                Logical NOT	
+and	                                AND	
+or	                                OR
 
 
 """
